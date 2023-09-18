af9670cd1a1035cae09d6d4fdcfa949a9999156b
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2023-09-18 12:35:22 -0400

    Added comments explaining each part of the code

--- script.js
+++ script.js
@@ -3,10 +3,16 @@
 	{Jorge Luis De Los Santos Reyes}
 */
 $(document).ready(function(){
+	//adding an event listener
 	$('#yearly-salary, #percent').on('keyup', ()=>{
+		//declaring and assigning values to variables
 		let salary = $("#yearly-salary").val();
 		let percent = $("#percent").val();
+
+		//Calculating and saving amount to spend into amountToSpend
 		let amountToSpend = Number(salary) * Number(percent) * 0.01;
+
+		//Replacing inner html of amount span to have the updated amountToSpend
 		$('#amount').text(`$${amountToSpend.toFixed(2)}`);
 	});
 });
