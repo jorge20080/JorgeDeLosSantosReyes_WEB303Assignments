6eaf93736bfb2212b64db62013657d4e49f57858
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2023-09-25 23:40:46 -0400

    Updated main js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 // WEB303 Assignment 2
 // Jorge De Los Santos Reyes - W0826009
+
+$(document).ready(function(){
+	$('#prospect, #convert, #retain').on('click', (e)=>{
+        $("#content").hide().empty();
+		var xhr = new XMLHttpRequest;
+        switch(e.target.id){
+            case "prospect":
+                xhr.open('GET','prospect.html',true);
+                break;
+            case "convert":
+                xhr.open('GET','convert.html',true);
+                break;
+            default:
+                xhr.open('GET','retain.html',true);
+                break;
+        }
+        xhr.onload = function() {
+            if(this.status=== 200) {
+                let el = document.getElementById("content");
+                el.innerHTML=xhr.responseText;
+            }
+        }   
+        $("#content").fadeIn(500);
+        xhr.send();
+	});
+});
